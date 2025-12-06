ab902fab9ebe3418972c10363dea9fe9229200eb
Anushkakadam14 <1403anushkakadam@gmail.com>
2025-12-06 11:55:20 +0530

    Delete problem1.py

--- problem1.py
+++
@@ -1,9 +0,0 @@
-#Q1. Write a program to take two numbers as input and print their sum, difference, product, and division
-
-a=int(input("Enter value of a:"))
-b=int(input("Enter value of b:"))
-
-print("sum", a+b)
-print("difference", a-b)
-print("product", a*b)
-print("division", a/b)
