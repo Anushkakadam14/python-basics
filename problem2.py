25ecd89a3872e9d6483fc80e0299e99bf98428fe
Anushkakadam14 <1403anushkakadam@gmail.com>
2025-12-06 11:55:34 +0530

    Delete problem2.py

--- problem2.py
+++
@@ -1,7 +0,0 @@
-# Program to find the remainder when a number is divided by 7
-
-number = int(input("Enter a number: "))
-
-remainder = number % 7
-
-print("Remainder when", number, "is divided by 7 is:", remainder)
